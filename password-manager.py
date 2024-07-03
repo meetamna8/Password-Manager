e5d64932718174c575da135829ca9326616f1926
meetamna8 <meetamna8@gmail.com>
2024-07-03 23:54:19 +0500

    commit

--- password-manager.py
+++ password-manager.py
@@ -1,3 +1,5 @@
+from cryptography.fernet import Fernet
+
 master_pwd = input("What is the master password? ")
 
 def view():
@@ -5,7 +7,7 @@ def view():
        for line in f.readlines():
            data = (line.rstrip())
            user , passw = data.split("|")
-           print(f"{user} | {passw}")
+           print(f"user : {user} | password: {passw}")
 
 def add():
     name = input("Account name: ")
@@ -17,12 +19,10 @@ def add():
 
 while True:
     mode = input("Would you like to add a new password or view existing ones (view / add) ? press q to quit. ").lower()
-    if mode == " q ":
+    if mode == "q":
         break
-        
     elif mode == "view":
         view()
-
     elif mode == "add":
         add()
     else:
